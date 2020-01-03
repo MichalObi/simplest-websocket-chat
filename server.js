f4741974c802dc5c540e17fa0193feca1511138f
MichalObi <ndwz123@gmail.com>
2020-01-03 18:26:09 +0100

    Pass unique id when sending msg

--- server.js
+++ server.js
@@ -3,26 +3,27 @@ const WebSocket = require("ws");
 const port = 8080;
 let server = null;
 
-console.log(`Server run on port: ${port}`);
+console.log(`Server run on port: ${port} !`);
 
 const sendMessage = (client, data) => {
-  if (client.readyState === WebSocket.OPEN) {
-    client.send(data);
-    console.log("message send");
-  }
+  if (client.readyState === WebSocket.OPEN) client.send(data);
 };
 
-const sendMessageToAll = data =>
+const sendMessageToAll = (data, senderId) => {
+  console.log(`message from ${senderId} send`);
   server.clients.forEach(client => sendMessage(client, data));
+};
 
-const handleMessage = socket => {
-  console.log("message rdy to send");
-  socket.on("message", data => sendMessageToAll(data));
+const handleMessage = (socket, req) => {
+  const senderId = req.headers["sec-websocket-key"];
+
+  console.log(`${senderId} connected`);
+  socket.on("message", data => sendMessageToAll(data, senderId));
 };
 
 const handleConnection = () => {
   console.log("connection established");
-  server.on("connection", socket => handleMessage(socket));
+  server.on("connection", (socket, req) => handleMessage(socket, req));
 };
 
 const startServer = () => {
